d3a76dac0d2c37944b9b105ec768af5c0e4f7862
Vraiment <jemc44@gmail.com>
2025-01-02 22:39:14 -0600

    Boilerplate

---
+++ lib/rspec/stubbed_environment_variables.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "stubbed_environment_variables/version"
+
+module Rspec
+  module StubbedEnvironmentVariables
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
